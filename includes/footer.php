611df89ca1996416c4fa9160f66c45d768cb3cac
kinami254 <patrickinami@gmail.com>
2021-02-20 10:31:57 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href=""><span>Facebook</span></a></li>
+				<li><a class="twitter" href="/"><span>Twitter</span></a></li>
+				<li><a class="instagram" href=""><span>Instagram</span></a></li>
+				
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2021 ESOVISVR TOURS . Brought To You By kinami Patrick </p>
+	</div>
+</div>
